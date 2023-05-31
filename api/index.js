e6b8e38adeeb339fb1cd387bf1df58313e1bfdb7
dinseangmeng <dinseangmeng@gmail.com>
2023-05-31 22:12:26 +0700

    Edit something

--- api/index.js
+++ api/index.js
@@ -29,8 +29,8 @@ const connectDB=async()=>{
         await mongoose.connect(`${process.env.DB_CONNECTION}`);
         console.log("CONNECTED");
       } catch (error) {
-        console.log("SOMETHING WHEN WRONG");
-        handleError(error);
+        // console.log("SOMETHING WHEN WRONG");
+        console.log(error);
       }
 }
 connectDB()
